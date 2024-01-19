8cb54895dd4fc059b26820938b3dcc3a71021888
Stephen Carter <stephen.carter@salesforce.com>
2024-01-19 14:39:55 -0500

    @W-14689540@: (Part 9) Implementing ui text review feedback

--- src/constants.ts
+++ src/constants.ts
@@ -6,28 +6,27 @@ export const INTERNAL_OUTFILE = 'SalesforceCodeAnalyzerResults.json'
 export const MESSAGES = {
     STEP_LABELS: {
         PREPARING_ENVIRONMENT: 'Preparing Environment',
-        RUNNING_CODE_ANALYZER: 'Running the Salesforce Code Analyzer',
+        RUNNING_CODE_ANALYZER: 'Running Salesforce Code Analyzer',
         UPLOADING_ARTIFACT: 'Uploading Artifact',
         ANALYZING_RESULTS: 'Analyzing Results',
         CREATING_SUMMARY: 'Creating Summary'
     },
-    MISSING_NORMALIZE_SEVERITY: `Missing required --normalize-severity option from run-arguments input.`,
+    MISSING_NORMALIZE_SEVERITY: `Missing required --normalize-severity argument from run-arguments input.`,
     SF_CLI_NOT_INSTALLED:
         `The sf command was not found.\n` +
-        `The Salesforce CLI must be installed in the environment to run the Salesforce Code Analyzer.\n` +
+        `The Salesforce CLI must be installed in the environment to run Salesforce Code Analyzer.\n` +
         `We recommend you include a separate step in your GitHub workflow to install it. For example:\n` +
-        `  - name: Install the Salesforce CLI\n` +
+        `  - name: Install Salesforce CLI\n` +
         `    run: npm install -g @salesforce/cli@latest\n` +
         `We will attempt to install the latest Salesforce CLI on your behalf.`,
-    SF_CLI_INSTALL_FAILED: `Failed to install the Salesforce CLI on your behalf.`,
+    SF_CLI_INSTALL_FAILED: `Failed to install the latest Salesforce CLI on your behalf.`,
     MINIMUM_SCANNER_PLUGIN_NOT_INSTALLED:
         `The @salesforce/sfdx-scanner plugin of version ${MIN_SCANNER_VERSION_REQUIRED} or greater was not found.\n` +
-        `The Salesforce Code Analyzer plugin of version ${MIN_SCANNER_VERSION_REQUIRED} or greater is required.\n` +
         `We recommend you include a separate step in your GitHub workflow to install it. For example:\n` +
-        `  - name: Install the Salesforce Code Analyzer plugin\n` +
+        `  - name: Install Salesforce Code Analyzer plugin\n` +
         `    run: sf plugins install @salesforce/sfdx-scanner@latest\n` +
-        `We will attempt to install the latest Salesforce Code Analyzer plugin on your behalf.`,
-    SCANNER_PLUGIN_INSTALL_FAILED: `Failed to install the latest Salesforce Code Analyzer plugin on your behalf.`
+        `We will attempt to install the latest @salesforce/sfdx-scanner plugin on your behalf.`,
+    SCANNER_PLUGIN_INSTALL_FAILED: `Failed to install the latest @salesforce/sfdx-scanner plugin on your behalf.`
 }
 export const MESSAGE_FCNS = {
     PLUGIN_FOUND: (pluginName: string, pluginVersion: string) =>
